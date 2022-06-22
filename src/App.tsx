1bf6f755b2fdfa775b743f1647cf85fd0fb31060
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-06-22 13:29:40 +0900

    styled-componentsの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Posts } from "./components/Posts";
-import "./App.css";
-
 
 const App = () => {
 
 
   return (
-    <div className="App">
+    <>
       <Posts />
-    </div>
+    </>
   );
 };
 
--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components"
 
 type Post = {
 	id: number;
@@ -19,17 +20,54 @@ export const Posts = () => {
 
 	return (
 		<>
-			<section className="sectionPosts">
+			<PostsSection>
 				<h3>POSTS</h3>
-				<ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <h4>{post.title}</h4>
-            <p>{post.body}</p>
-          </li>
-        ))}
-      </ul>
-			</section>
+				<PostsList>
+					{posts.map((post) => (
+						<li key={post.id}>
+							<h4>{post.title}</h4>
+							<p>{post.body}</p>
+						</li>
+					))}
+				</PostsList>
+			</PostsSection>
 		</>
 	)
 }
+
+const PostsSection = styled.section`
+	width: 90%;
+	margin: 20px auto;
+	display: flex;
+	flex-direction: column;
+	gap: 8px;
+
+	h3 {
+		color: #333;
+		font-size: 1.5em;
+		margin: 0;
+		margin-bottom: 12px;
+		padding: 0;
+	}
+`;
+
+const PostsList = styled.ul`
+	list-style: none;
+	margin: 0;
+	padding: 0;
+
+	li {
+		background-color: #fff;
+		border-radius: 2px;
+		box-shadow: 0 2px 3px rgba(0, 0, 0, 0.125);
+		margin: 0 0 1rem 0;
+		padding: 1rem;
+
+		h4 {
+			color: #333;
+			font-size: 1.2em;
+			margin: 0;
+			padding: 0;
+		}
+	}
+`;
