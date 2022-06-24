d80521c15ab0a835062971019c97cf63dcb0ebde
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-06-24 19:44:37 +0900

    削除機能の追加

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -14,25 +14,41 @@ export const Posts = memo(() => {
 	const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3000/posts')
-			.then((response) => {
-				setPosts(response.data);
-			})
-			.catch(error => alert('Error'))
+    GetPost();
   }, []);
+
+	const GetPost = () => {
+		axios.get('http://localhost:3000/posts')
+		.then((response) => {
+			setPosts(response.data);
+		})
+		.catch(error => alert('Error'))
+	}
+
+	const deletePost = (id: number) => {
+		axios.delete(`http://localhost:3000/posts/${id}`)
+			.then((response) => {
+				alert('投稿を削除しました');
+				GetPost();
+			})
+			.catch(error => alert('削除に失敗しました'))
+	}
 
 	return (
 		<>
 			<PostsSection>
 				<h3>POSTS</h3>
-				<PostsList>
-					{posts.map((post) => (
-						<li key={post.id}>
-							<h4>{post.title}</h4>
-							<p>{post.body}</p>
-						</li>
-					))}
-				</PostsList>
+				{posts.map((post) => (
+					<PostsList key={post.id}>
+						<div>
+							<li>
+								<h4>{post.title}</h4>
+								<p>{post.body}</p>
+							</li>
+						</div>
+						<img src="../src/images/delete.png" alt="削除" onClick={() => deletePost(post.id)} />
+					</PostsList>
+				))}
 			</PostsSection>
 		</>
 	)
@@ -55,22 +71,26 @@ const PostsSection = styled.section`
 `;
 
 const PostsList = styled.ul`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
 	list-style: none;
-	margin: 0;
-	padding: 0;
+	background-color: #fff;
+	border-radius: 2px;
+	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.125);
+	margin: 0 0 1rem 0;
+	padding: 1rem;
 
-	li {
-		background-color: #fff;
-		border-radius: 2px;
-		box-shadow: 0 2px 3px rgba(0, 0, 0, 0.125);
-		margin: 0 0 1rem 0;
-		padding: 1rem;
+	h4 {
+		color: #333;
+		font-size: 1.2em;
+		margin: 0;
+		padding: 0;
+	}
 
-		h4 {
-			color: #333;
-			font-size: 1.2em;
-			margin: 0;
-			padding: 0;
-		}
+	img {
+		width: 2.5%;
+		object-fit: contain;
+		margin-right: 30px;
 	}
 `;
