8e348cd9c79393ed0c354de9e9c200b387113f35
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-06-29 17:46:51 +0900

    Homeコンポーネントでの制限確認削除

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,25 +1,13 @@
-import { useContext } from "react";
-import { AuthContext } from "./providers/AuthProvider";
 import { Header } from "./components/Header";
 import { Form } from "./components/Form";
 import { Posts } from "./components/Posts";
-import { Navigate } from "react-router-dom";
 
 export const Home = () => {
-  const {currentUser} = useContext(AuthContext);
-
-  // ログインユーザーがいる場合のみHomeにアクセス
   return (
     <>
-      {currentUser ? (
-        <>
-          <Header />
-          <Form />
-          <Posts />
-        </>
-      ) : (
-        <Navigate to="/signin" />
-      )}
+      <Header />
+      <Form />
+      <Posts />
     </>
   );
 };
